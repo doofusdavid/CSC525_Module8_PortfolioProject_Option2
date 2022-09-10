abf1e0b08545e4d69b1ff40e94459715750ddf58
David Edwards <david.edwards@gmail.com>
2022-09-10 12:24:08 -0600

    everything else

---
+++ conversation.py
@@ -0,0 +1,35 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Uncomment the following lines to enable verbose logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+bot = ChatBot(
+    'Terminal',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=['chatterbot.logic.BestMatch'],
+    database_uri='sqlite:///database.db',
+    logic_aa=['chatterbot.logic.MathematicalEvaluation',
+              'chatterbot.logic.TimeLogicAdapter',
+              'chatterbot.logic.BestMatch'],
+)
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train(
+    'chatterbot.corpus.english'
+)
+print('Type something to begin...')
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        user_input = input()
+
+        bot_response = bot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
